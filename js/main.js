1b256ce96595fb0689857f378f568223a0f166fc
Caetano <gmengarelliyespersen@alumnos.exa.unicen.edu.ar>
2023-10-04 11:55:33 -0300

    carrusel y botones

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@
 let arrowRigth = document.querySelector('.arrowRigth');
 let arrowLeft = document.querySelector('.arrowLeft');
 let contenedorPrincipal = document.querySelector('.contenedorPrincipal');
+let posicion = 0;
+
+arrowRigth.addEventListener('click', moveRigth);
 
 
-arrowRigth.addEventListener('click', moverCarrusel);
-
-function moverCarrusel(){
-    contenedorPrincipal.classList.add('carruselClickRigth');
-
-    arrowRigth.removeEventListener('click', moverCarrusel);
+function moveRigth(){
+    if(posicion>-53){
+        posicion -= 27;
+        contenedorPrincipal.style.transform = `translateX(${ posicion }%)`;
+    }
 }
 
+arrowLeft.addEventListener('click', moveLeft);
 
+function moveLeft(){
+    if(posicion<-1){
+        posicion += 27;
+        contenedorPrincipal.style.transform = `translateX(${ posicion }%)`;
+    }
 
+}
