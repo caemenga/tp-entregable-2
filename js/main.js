c6dd269e147a4558422260132db4a3c7b1bc0852
Caetano <gmengarelliyespersen@alumnos.exa.unicen.edu.ar>
2023-10-08 19:36:51 -0300

    avanzando

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ let posiciones = {};
 document.querySelectorAll('.arrowRigth').forEach(function (arrowRigth, index) {
   let  arrowLeft = document.querySelectorAll('.arrowLeft')[index];
   let contenedorPrincipal = contenedoresPrincipales[index];
-  let card = document.querySelectorAll(".card")[index];
+  
 
 
   posiciones[index] = 0;
 
   arrowRigth.addEventListener('click', function () {
     moveRigth(index);
-    card.classList.toggle('cardScroll');
+    
   });
 
   arrowLeft.addEventListener('click', function () {
@@ -28,12 +28,15 @@ function moveRigth(index) {
   if (posiciones[index] > -53) {
     posiciones[index] -= 27;
     contenedoresPrincipales[index].style.transform = `translateX(${posiciones[index]}%)`;
+    let card = document.querySelectorAll(".card")[index];
   }
 }
 
 function moveLeft(index) {
   if (posiciones[index] < -1) {
     posiciones[index] += 27;
+    let card = document.querySelectorAll(".card")[index];
     contenedoresPrincipales[index].style.transform = `translateX(${posiciones[index]}%)`;
+    card.classList.toggle('cardScroll');
   }
 }
